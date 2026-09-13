6e8e68d7782c0b71fb1fbfa41a27b5086ba2896b
Sasha6756 <74523716+Sasha6756@users.noreply.github.com>
2026-09-13 14:42:27 +1000

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI 
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import get_settings
